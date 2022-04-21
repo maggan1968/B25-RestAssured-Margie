4a038b743aea69eb4e98aca15deea6d3445f077b
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-21 09:11:55 -0500

    day2

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -18,7 +19,7 @@ public class SpartanGetRequests {
         And response content type must be application/json
      */
     @Test
-    public void test1(){
+    public void test1() {
 
         Response response = RestAssured.
                 given().
@@ -32,10 +33,10 @@ public class SpartanGetRequests {
 
         //how to test API ?
         //verify status code is 200
-        Assertions.assertEquals(200,response.statusCode());
+        Assertions.assertEquals(200, response.statusCode());
 
         //verify content type is application json
-        Assertions.assertEquals("application/json",response.contentType());
+        Assertions.assertEquals("application/json", response.contentType());
 
 
     }
@@ -48,7 +49,7 @@ public class SpartanGetRequests {
      */
 
     @Test
-    public void test2(){
+    public void test2() {
 
         Response response = RestAssured.
                 given().
@@ -57,16 +58,52 @@ public class SpartanGetRequests {
                 .get(url + "/api/spartans/3");
         //how to test API ?
         //verify status code is 200
-        Assertions.assertEquals(200,response.statusCode());
+        Assertions.assertEquals(200, response.statusCode());
 
         //verify content type is application json
-        Assertions.assertEquals("application/json",response.contentType());
+        Assertions.assertEquals("application/json", response.contentType());
 
-       //verify Fidole exist in json body
+        //verify Fidole exist in json body
         Assertions.assertTrue(response.body().asString().contains("Fidole"));
 
 
+    }
 
+    /*
+        Given no headers provided
+        When Users send GET request to /api/hello
+        Then response status code should be 200
+        And Content type header should be “text/plain;charset=UTF-8”
+        And header should contain date
+        And Content-Length should be 17
+        And body should be “Hello from Sparta"
+        */
+    @DisplayName("GET request to /api/hello endpoint")
+    @Test
+    public void test3() {
 
+        Response response = RestAssured.when().get(url + "/api/hello");
+
+        response.prettyPrint();
+
+        //verify status code
+        Assertions.assertEquals(200, response.statusCode());
+
+        //verify content type
+        Assertions.assertEquals("text/plain;charset=UTF-8", response.contentType());
+
+        //verify Date header exists in Response headers
+        //we use hasHeaderWithName method to verify header exists or not - it returns boolean
+        Assertions.assertTrue(response.headers().hasHeaderWithName("Date"));
+
+        //to get header value we use header() method which accepts header name as parameter and return value as string
+        System.out.println("response.header(\"Content-Length\") = " + response.header("Content-Length"));
+        System.out.println("response.header(\"Connection\") = " + response.header("Connection"));
+
+        //verify content length is 17
+        Assertions.assertEquals("17", response.header("Content-Length"));
+
+        //verify body is "hello from sparta"
+        Assertions.assertEquals("Hello from Sparta", response.body().asString());
     }
 }
