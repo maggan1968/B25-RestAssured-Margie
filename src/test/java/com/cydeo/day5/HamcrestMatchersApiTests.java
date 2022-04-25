1d9f4f418c3bb95f5f55073537a854d94e3e95b4
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-25 14:49:37 -0500

    day5 HamcrestMatchersApiTests

---
+++ src/test/java/com/cydeo/day5/HamcrestMatchersApiTests.java
@@ -0,0 +1,84 @@
+package com.cydeo.day5;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class HamcrestMatchersApiTests {
+
+     /*
+       given accept type is Json
+       And path param id is 15
+       When user sends a get request to spartans/{id}
+       Then status code is 200
+       And content type is Json
+       And json data has following
+           "id": 15,
+           "name": "Meta",
+           "gender": "Female",
+           "phone": 1938695106
+        */
+
+    @DisplayName("one Spartan with Hamcrest and Chaining")
+    @Test
+    public void  test1(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParams("id",15)
+                .when()
+                .get("http://3.83.127.158:8000/api/spartans/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json")
+                .and()
+                .body("id",is(15),
+                        "name",is("Meta"),
+                        "gender",equalTo("Female"),
+                        "phone",is(1938695106));
+
+
+    }
+    @DisplayName("CBTraining Teacher request with chaining and matchers")
+    @Test
+    public void test2(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("id",10423)
+                .when()
+                .get("http://api.cybertektraining.com/teacher/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                .and()
+                .header("Content-Encoding","gzip")
+                .and()
+                .header("Date",notNullValue())
+                .body("teachers[0].firstName",is("Alexander"),
+                        "teachers[0].lastName",is("Syrup"),
+                        "teachers[0].gender",equalTo("male"));
+
+    }
+
+    @Test
+    public void test3(){
+        //verify "Candi","Alexander","Francesca" inside the all teachers
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("http://api.cybertektraining.com/teacher/all")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("teachers.firstName",hasItems("Candi","Alexander","Francesca"));
+    }
+
+
+    }
+
