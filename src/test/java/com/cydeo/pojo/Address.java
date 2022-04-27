b94819cbeae5d5adffc7f3b4a08fda40b998a168
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-27 07:22:23 -0500

    day6 CTrainingPojoTest update1 is done

---
+++ src/test/java/com/cydeo/pojo/Address.java
@@ -0,0 +1,14 @@
+package com.cydeo.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Address {
+
+    private String street;
+    private int zipCode;
+    private String state;
+}
