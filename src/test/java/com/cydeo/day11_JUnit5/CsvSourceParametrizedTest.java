3fbd463227e01751c41bfaa1e2b922c9a82b6e4b
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-05-09 09:26:10 -0500

    day11 notes.txt

--- src/test/java/com/cydeo/day11_JUnit5/CsvSourceParametrizedTest.java
+++ src/test/java/com/cydeo/day11_JUnit5/CsvSourceParametrizedTest.java
@@ -1,4 +1,78 @@
 package com.cydeo.day11_JUnit5;
 
+import io.restassured.http.ContentType;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class CsvSourceParametrizedTest {
+
+    // Test first number + second number = third number
+    //         1,             3,             4
+    //         2,             3,             5
+    //         8,             7,             15
+
+    @ParameterizedTest
+    @CsvSource({"1, 3, 4",
+            "2, 3, 5",
+            "8, 7, 15",
+            "10, 9, 19"})
+    public void  testAddition(int num1, int num2, int sum) {
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("sum = " + sum);
+        MatcherAssert.assertThat(num1+num2, Matchers.equalTo(sum));
+    }
+
+    // Write a parameterized test for this request
+    // GET https://api.zippopotam.us/us/{state}/{city}
+    /*
+        "NY, New York",
+        "CO, Denver",
+        "VA, Fairfax",
+        "VA, Arlington",
+        "MA, Boston",
+        "NY, New York",
+        "MD, Annapolis"
+     */
+    //verify place name contains your city name
+    //print number of places for each request
+
+    @ParameterizedTest
+    @CsvSource({ "NY, New York",
+            "CO, Denver",
+            "VA, Fairfax",
+            "VA, Arlington",
+            "MA, Boston",
+            "NY, New York",
+            "MD, Annapolis"})
+    public void zipCodeTest(String state,String city){
+
+        System.out.println("state = " + state);
+        System.out.println("city = " + city);
+
+        int placeNumber = given()
+                .baseUri("https://api.zippopotam.us")
+                .accept(ContentType.JSON)
+                .pathParam("state",state)
+                .and()
+                .pathParam("city",city)
+                .when().get("/us/{state}/{city}")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("places.'place name'",everyItem(containsStringIgnoringCase(city)))
+                .extract().jsonPath().getList("places").size();
+
+        System.out.println("placeNumber = " + placeNumber);
+
+    }
+
+
+
+
 }
